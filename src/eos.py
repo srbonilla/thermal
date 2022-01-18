e7f017a06339150b9e8441cb7ad84db1dc3ba025
Sergio Ruiz-Bonilla <dc-ruiz1@login7b.pri.cosma7.alces.network>
2022-01-18 14:10:35 +0000

    load vapor and melt curves

---
+++ src/eos.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+class EOSvaporcurve:
+    """Class for vapor curve from ANEOS."""
+    def __init__(self):
+        self.A1_T = None
+        self.A1_rho_liq = None
+        self.A1_rho_vap = None
+        self.A1_P_liq = None
+        self.A1_P_vap = None
+        self.A1_u_liq = None
+        self.A1_u_vap = None
+        self.A1_s_liq = None
+        self.A1_s_vap = None
+        self.units = ''
+
+class EOSmeltcurve:
+    """Class for melt curve from ANEOS."""
+    def __init__(self):
+        self.A1_T  = None
+        self.A1_rho_liq = None
+        self.A1_rho_sol = None
+        self.A1_P_liq = None
+        self.A1_P_sol = None
+        self.A1_u_liq = None
+        self.A1_u_sol = None
+        self.A1_s_liq = None
+        self.A1_s_sol = None
+        self.units = ''
+        
+class EOScriticalpoint:
+    """Class for critical point state from the EOS."""
+    def __init__(self):
+        self.P   = 0
+        self.s   = 0  
+        self.T   = 0 
+        self.rho = 0
+        self.u   = 0
+        self.units = ''
+        
+def read_eosvaporcurve(filepath):
+    vc = EOSvaporcurve()
+    with open(filepath) as f:
+        lines = f.readlines()
+
+    vc.units = lines[1][:-1]
+    vc.A1_T = np.array(lines[2].split(", ")[:-1], dtype='float')
+    vc.A1_rho_liq = np.array(lines[3].split(", ")[:-1], dtype='float')
+    vc.A1_rho_vap = np.array(lines[4].split(", ")[:-1], dtype='float')
+    vc.A1_P_liq = np.array(lines[5].split(", ")[:-1], dtype='float')
+    vc.A1_P_vap = np.array(lines[6].split(", ")[:-1], dtype='float')
+    vc.A1_u_liq = np.array(lines[7].split(", ")[:-1], dtype='float')
+    vc.A1_u_vap = np.array(lines[8].split(", ")[:-1], dtype='float')
+    vc.A1_s_liq = np.array(lines[9].split(", ")[:-1], dtype='float')
+    vc.A1_s_vap = np.array(lines[10].split(", ")[:-1], dtype='float')
+    
+    return vc
+
+def read_eosmeltcurve(filepath):
+    mc = EOSmeltcurve()
+    with open(filepath) as f:
+        lines = f.readlines()
+
+    mc.units = lines[1][:-1]
+    mc.A1_T = np.array(lines[2].split(", ")[:-1], dtype='float')
+    mc.A1_rho_liq = np.array(lines[3].split(", ")[:-1], dtype='float')
+    mc.A1_rho_sol = np.array(lines[4].split(", ")[:-1], dtype='float')
+    mc.A1_P_liq = np.array(lines[5].split(", ")[:-1], dtype='float')
+    mc.A1_P_sol = np.array(lines[6].split(", ")[:-1], dtype='float')
+    mc.A1_u_liq = np.array(lines[7].split(", ")[:-1], dtype='float')
+    mc.A1_u_sol = np.array(lines[8].split(", ")[:-1], dtype='float')
+    mc.A1_s_liq = np.array(lines[9].split(", ")[:-1], dtype='float')
+    mc.A1_s_sol = np.array(lines[10].split(", ")[:-1], dtype='float')
+    
+    return mc
+        
+ANEOS_forsterite_cp = EOScriticalpoint()
+ANEOS_forsterite_cp.__dict__ = {'P': 0.159304, 's': 0.00637921, 'T': 6043.58, 'rho': 570.535, 'u': 16.9138, 'units': 'T K, rho kg/m3, P GPa, U MJ/kg, S MJ/K/kg'}
+
+ANEOS_Fe85Si15_cp = EOScriticalpoint()
+ANEOS_Fe85Si15_cp.__dict__ = {'P': 0.540777, 's': 0.00402756, 'T': 7279.36, 'rho': 857.1319999999999, 'u': 11.0937, 'units': 'T K, rho kg/m3, P GPa, U MJ/kg, S MJ/K/kg'}
+
+ANEOS_forsterite_vc = read_eosvaporcurve("./data/ANEOS_forsterite_vc.txt")
+ANEOS_forsterite_mc = read_eosmeltcurve("./data/ANEOS_forsterite_mc.txt")
+ANEOS_Fe85Si15_vc = read_eosvaporcurve("./data/ANEOS_Fe85Si15_vc.txt")
+ANEOS_Fe85Si15_mc = read_eosmeltcurve("./data/ANEOS_Fe85Si15_mc.txt")
+    
+    
+def plot_ANEOS_rhou(ax, mat, **kwargs):
+    
+    lw = kwargs.get("lw", 1)
+    
+    if mat == "ANEOS_forsterite":
+        vc = ANEOS_forsterite_vc
+        mc = ANEOS_forsterite_mc
+    elif mat == "ANEOS_Fe85Si15":
+        vc = ANEOS_Fe85Si15_vc
+        mc = ANEOS_Fe85Si15_mc
+    else:
+        raise ValueError("Unknown material")
+        
+    ax.plot(vc.A1_rho_liq, vc.A1_u_liq, color="black", linewidth=lw)
+    ax.plot(vc.A1_rho_vap, vc.A1_u_vap, color="black", linewidth=lw)
+    ax.plot(vc.A1_rho_liq[0], vc.A1_u_liq[0],'ko')
+    ax.plot(mc.A1_rho_liq, mc.A1_u_liq, color="black", linewidth=lw)
+    ax.plot(mc.A1_rho_sol, mc.A1_u_sol, color="black", linewidth=lw)
+    
+def plot_ANEOS_sP(ax, mat, **kwargs):
+    
+    lw = kwargs.get("lw", 1)
+    
+    if mat == "ANEOS_forsterite":
+        vc = ANEOS_forsterite_vc
+        mc = ANEOS_forsterite_mc
+    elif mat == "ANEOS_Fe85Si15":
+        vc = ANEOS_Fe85Si15_vc
+        mc = ANEOS_Fe85Si15_mc
+    else:
+        raise ValueError("Unknown material")
+        
+    ax.plot(vc.A1_s_liq, vc.A1_P_liq, color="black", linewidth=lw)
+    ax.plot(vc.A1_s_vap, vc.A1_P_vap, color="black", linewidth=lw)
+    ax.plot(vc.A1_s_liq[0], vc.A1_P_liq[0],'ko')
+    ax.plot(mc.A1_s_liq, mc.A1_P_liq, color="black", linewidth=lw)
+    ax.plot(mc.A1_s_sol, mc.A1_P_sol, color="black", linewidth=lw)
